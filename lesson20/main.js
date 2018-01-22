c1c1046c5209231fc6e16a09c2de1b0808d3192e
Denis Solovyov <solovyov.denis@gmail.com>
2018-01-22 18:48:34 +0200

    lesson20

--- lesson20/main.js
+++ lesson20/main.js
@@ -3,11 +3,27 @@ console.log('work20');
 var text = document.getElementById('id1');
 var i = 9;
 var vector = -1;
-var timer1 = setInterval(function(){
-    console.log(i);
-    text.style.opacity = i / 10;
-    if (i == 0 || i == 10){
-        vector *= -1;
-    }
-    i += 1*vector;
-}, 100);
+// var timer1 = setInterval(function(){
+//     console.log(i);
+//     text.style.opacity = i / 10;
+//     if (i == 0 || i == 10){
+//         vector *= -1;
+//     }
+//     i += 1*vector;
+// }, 100);
+
+
+// var color, j = 0;
+// var timer1 = setInterval(function(){
+//     color = j.toString(16);
+//     color = '#' + '0'.repeat(6 - color.length) + color;
+//     text.style.color = color;
+//     if (j >= 16777215){
+//         clearInterval(timer1);
+//     }
+//     j += 53261;
+// }, 200);
+
+
+
+
