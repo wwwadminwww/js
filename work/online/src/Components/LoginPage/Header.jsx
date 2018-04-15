78cc16ce7fa97e65e492e525d4c58137d26be8a0
Denis Solovyov <solovyov.denis@gmail.com>
2018-04-15 14:04:56 +0300

    add work

---
+++ work/online/src/Components/LoginPage/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div class="row">
+            <a href="http://platio.uk" target="_blank"><img class="logo" src="https://www.platio.uk/images/logo.png" /></a>
+        </div>
+    )
+}
+
+export default Header;
---
+++ work/online/src/Components/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function LoginPage(){
+    return (
+        <div className="wrap">
+            <Header />
+        </div>
+    )
+}
+
+export default LoginPage;
